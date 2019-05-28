9b5e74945430796e331cdbcf39373ca690b920fd
sierratango34 <k.sritawan@gmail.com>
2019-05-28 16:29:40 +0100

    cloned from Matt in lab

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PokeBox from './conteiners/PokeBox';
+
+class App extends Component {
+  render() {
+    return (
+      <PokeBox />
+    );
+  }
+}
+
+export default App;
